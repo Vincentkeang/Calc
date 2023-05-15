2895b45756530007fb2cdd1728a83e2e60c27b20
Vincent keang <75630071+Vincentkeang@users.noreply.github.com>
2023-05-15 12:42:35 -0400

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,4 +1,6 @@
 # A simple python program to calculate an addition subtraction etc 
+import turtle 
+import os 
 
 def addition(choice):
     if choice == 1:
@@ -39,30 +41,39 @@ def division(choice):
 
         total = num1 // num2 
         return print(f"The total of {num1} divided {num2} is {total}")
+    
 
 
+# Opens a new window for the calculator 
+wn = turtle.Screen()
+wn.bgcolor("white")
+wn.setup(width=800, height=600)
+wn.tracer(0)
 
+# Number 1 for numpad 
+Num1 = turtle.Turtle() 
+Num1.shape("square")
+Num1.color("orange")
+Num1.shapesize(stretch_wid=5, stretch_len=1)
+Num1.penup()
+# UI here when finished 
 
-def main():
-    print("What would you like me to do: ")
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3. Multiplication")
-    print("4. Division")
-    choice = int(input("> "))
-    if choice ==  1:
-        print("You have chose addition! ")
-        addition(choice) 
-    elif choice == 2:
-        print("You have chose Subtraction")
-        subtract(choice)
-    elif choice == 3:
-        print("You have chose Multiplication")
-        multiply(choice)
-    elif choice == 4:
-        print("You have chose Division")
-        division(choice)
-
-
-
-main()
+print("What would you like me to do: ")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Factor ")
+choice = int(input("> "))
+if choice ==  1:
+    print("You have choose addition! ")
+    addition(choice) 
+elif choice == 2:
+    print("You have choose Subtraction")
+    subtract(choice)
+elif choice == 3:
+    print("You have choose Multiplication")
+    multiply(choice)
+elif choice == 4:
+    print("You have choose Division")
+    division(choice)
